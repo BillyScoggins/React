068a484e4f9f65a649e569e9824d9f9ff4a88780
Billy Scoggins <118499026+BillyScoggins@users.noreply.github.com>
2023-07-24 14:36:44 -0500

    updated nav tabs

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Billy Scoggins - I am new to programming and developing and enjoying the challenge. 
+        Programming is a complete career change for me. I've always been fascinated by 
+        learning new things.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>
+        Billy Scoggins
+        C- 214.507.3251
+        E- billy_scoggins@yahoo.com
+
+        
+      </p>
+    </div>
+  );
+}
